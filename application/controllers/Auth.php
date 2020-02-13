a118e3aba2909edd5394153312337624c9251a35
mnali220594 <muhammadnurali303@gmail.com>
2020-02-13 14:34:08 +0700

    [Ali] Edit login controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,26 +10,71 @@ class Auth extends CI_Controller
   }
   public function index()
   {
-    $data['title'] = 'Login Page';
+    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
-    $this->load->view('templates/auth_header', $data);
-    $this->load->view('auth/login');
-    $this->load->view('templates/auth_footer');
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'Login Page';
+
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/login');
+      $this->load->view('templates/auth_footer');
+    } else {
+      // validasinya success
+      $this->_login();
+    }
+  }
+
+  private function _login()
+  {
+    $email = $this->input->post('email');
+    $password = $this->input->post('password');
+
+    $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+    // usernya ada
+    if ($user) {
+      // jika usernya aktif
+      if ($user['is_active'] == 1) {
+        // cek password
+        if (password_verify($password, $user['password'])) {
+          $data = [
+            'email' => $user['email'],
+            'role_id' => $user['role_id']
+          ];
+          $this->session->set_userdata($data);
+          redirect('user');
+        } else {
+          $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email or password does not correct</div>');
+          redirect('auth');
+        }
+      } else {
+        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Your account has not been activated! Please activate it first</div>');
+        redirect('auth');
+      }
+    } else {
+      $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email or password doesnt correct</div>');
+      redirect('auth');
+    }
   }
 
   public function registration()
   {
     // Set registration rules
     $this->form_validation->set_rules('name', 'Name', 'required|trim');
+
     $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
       'is_unique' => 'This email already existed! Please register with other email'
     ]);
+
     $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
       'matches' => 'Password doesnt match!',
       'min_length' => 'Password too short!'
     ]);
+
     $this->form_validation->set_rules('password2', 'Password Confirmation', 'required|trim|matches[password1]');
 
+    // Check form validation
     if ($this->form_validation->run() == false) {
       $data['title'] = 'User Registration';
       $this->load->view('templates/auth_header', $data);
@@ -37,8 +82,8 @@ class Auth extends CI_Controller
       $this->load->view('templates/auth_footer');
     } else {
       $data = [
-        'name' => htmlspecialchars($this->input->post('name')),
-        'email' => htmlspecialchars($this->input->post('email')),
+        'name' => htmlspecialchars($this->input->post('name', true)),
+        'email' => htmlspecialchars($this->input->post('email', true)),
         'image' => 'default.jpg',
         'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
         'role_id' => 2,
@@ -50,5 +95,13 @@ class Auth extends CI_Controller
       $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation your account has been created. Please login</div>');
       redirect('auth');
     }
+  }
+
+  public function logout()
+  {
+    $this->session->unset_userdata('email');
+    $this->session->unset_userdata('role_id');
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out</div>');
+    redirect('auth');
   }
 }

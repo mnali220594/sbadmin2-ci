3e59520573de70d490740e22d7465eedb45bb794
mnali220594 <muhammadnurali303@gmail.com>
2020-02-15 23:24:52 +0700

    [Ali] add edit-user controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,6 +10,10 @@ class Auth extends CI_Controller
   }
   public function index()
   {
+    if ($this->session->userdata('email')) {
+      redirect('user');
+    }
+
     $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
     $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
